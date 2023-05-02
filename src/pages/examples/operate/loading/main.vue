06d8cdc9a5c51227516c1df2f7ff38b921478254
beck <2223969110@qq.com>
2023-05-02 11:31:52 +0800

    chore: 完善 Loading 组件演示

--- src/pages/examples/operate/loading/main.vue
+++ src/pages/examples/operate/loading/main.vue
@@ -6,89 +6,46 @@ const toPage = (name: string) => {
 
 <template>
     <sd-page title="Loading 加载动画">
-        <view class="demo-card">
-            <view class="demo-card__title">
-                基本用法
-            </view>
-            <view class="demo-card__body">
-                <sd-loading />
-            </view>
-        </view>
+        <demo-card title="基本用法">
+            <sd-loading />
+        </demo-card>
 
-        <view class="demo-card">
-            <view class="demo-card__title">
-                显示横向文本
-            </view>
-            <view class="demo-card__body">
-                <sd-loading text="正在加载中" />
-            </view>
-        </view>
+        <demo-card title="显示横向文本">
+            <sd-loading text="正在加载中" />
+        </demo-card>
 
-        <view class="demo-card">
-            <view class="demo-card__title">
-                显示纵向文本
-            </view>
-            <view class="demo-card__body">
-                <sd-loading text="正在加载中" direction="column" />
-            </view>
-        </view>
+        <demo-card title="显示纵向文本">
+            <sd-loading text="正在加载中" direction="column" />
+        </demo-card>
 
-        <view class="demo-card">
-            <view class="demo-card__title">
-                自定义颜色
-            </view>
-            <view class="demo-card__body">
-                <sd-loading color="#09BE4F" />
-            </view>
-        </view>
+        <demo-card title="自定义颜色">
+            <sd-loading color="#09BE4F" />
+        </demo-card>
 
-        <view class="demo-card">
-            <view class="demo-card__title">
-                音符动画
-            </view>
-            <view class="demo-card__body">
-                <sd-loading type="line" color="#09BE4F" />
-            </view>
-        </view>
+        <demo-card title="音符动画">
+            <sd-loading type="line" color="#09BE4F" />
+        </demo-card>
 
-        <view class="demo-card">
-            <view class="demo-card__title">
-                相遇动画
-            </view>
-            <view class="demo-card__body">
-                <sd-loading type="meet" color="#09BE4F" />
-            </view>
-        </view>
+        <demo-card title="相遇动画">
+            <sd-loading type="meet" color="#09BE4F" />
+        </demo-card>
 
-        <view class="demo-card">
-            <view class="demo-card__title">
-                页面 Loading (一)
-            </view>
-            <view class="demo-card__body" style="text-align: center;">
-                <sd-button type="primary" :custom-style="{ width: '70%' }" variant="plain" @click="toPage('page-circle')">
-                    应用
-                </sd-button>
-            </view>
-        </view>
-        <view class="demo-card">
-            <view class="demo-card__title">
-                页面 Loading (二)
-            </view>
-            <view class="demo-card__body" style="text-align: center;">
-                <sd-button type="primary" :custom-style="{ width: '70%' }" variant="plain" @click="toPage('page-line')">
-                    应用
-                </sd-button>
-            </view>
-        </view>
-        <view class="demo-card">
-            <view class="demo-card__title">
-                页面 Loading (三)
-            </view>
-            <view class="demo-card__body" style="text-align: center;">
-                <sd-button type="primary" :custom-style="{ width: '70%' }" variant="plain" @click="toPage('page-meet')">
-                    应用
-                </sd-button>
-            </view>
-        </view>
+        <demo-card title="页面 Loading (一)" body-class="sd-flex-x-center">
+            <sd-button width="70%" variant="plain" @click="toPage('page-circle')">
+                应用
+            </sd-button>
+        </demo-card>
+
+        <demo-card title="页面 Loading (二)" body-class="sd-flex-x-center">
+            <sd-button width="70%" variant="plain" @click="toPage('page-line')">
+                应用
+            </sd-button>
+        </demo-card>
+
+        <demo-card title="页面 Loading (三)" body-class="sd-flex-x-center">
+            <sd-button width="70%" variant="plain" @click="toPage('page-meet')">
+                应用
+            </sd-button>
+        </demo-card>
     </sd-page>
 </template>
