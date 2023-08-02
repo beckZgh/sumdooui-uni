a4d49f2068408f5d9efa19ad7f79d7b97513ec08
beck <2223969110@qq.com>
2023-08-02 21:06:16 +0800

    feat: 支持 install 写入配置、及主题自定义

---
+++ src/sumdooui-uni/config/index.ts
@@ -0,0 +1,6 @@
+
+import { reactive } from 'vue'
+export const config = reactive({
+    theme_vars: {} as Record<string, any>,
+})
+
---
+++ src/sumdooui-uni/index.ts
@@ -0,0 +1,21 @@
+
+import type { App } from 'vue'
+import { config } from './config'
+
+export * from './page/page'
+
+export function setThemeVars(vars: Record<string, any>) {
+    config.theme_vars = vars
+}
+
+interface Options {
+    theme_vars?: Record<string, any> // 主题变量 (依赖页面组件)
+    locale?: string                  // 默认语言
+}
+
+export default function install(app: App, options?: Options) {
+    // 设置组件主题
+    if (options?.theme_vars) {
+        config.theme_vars = options.theme_vars
+    }
+}
